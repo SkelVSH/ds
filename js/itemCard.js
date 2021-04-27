634fcf0c745b7c853bec6d65aa74b4d310d37eff
SkelVSH <51212766+SkelVSH@users.noreply.github.com>
2021-04-27 14:50:59 +0300

    feat: desktop floating block ready

--- js/itemCard.js
+++ js/itemCard.js
@@ -1,8 +1,7 @@
-
 /* Скрипт для тестирования формы для быстрого заказа. При переносе нужно подогнать форму под стандарт сайта и удалить этот скрипт! */
-(fastOrder = () => {
-  const form = document.querySelector('.card-config-main-form')
-  const success = document.querySelector('.card-config-main-form-success')
+fastOrder = (block) => {
+  const form = block.querySelector('.card-config-main-form')
+  const success = block.querySelector('.card-config-main-form-success')
   const input = form.querySelector('.card-config-main-form__input')
   const submitButton = form.querySelector('.card-config-main-form__submit')
   input.addEventListener('keyup', () => {
@@ -18,10 +17,10 @@
     form.style.display = 'none'
     success.style.display = 'flex'
   })
-})();
+}
 
-(rotatePhoto = () => {
-  const photoWrap = document.querySelector('.card-config-img')
+rotatePhoto = (block) => {
+  const photoWrap = block.querySelector('.card-config-img')
   const swapButton = photoWrap.querySelector('.card-config-img-turn__svg')
   const backImg = photoWrap.querySelector('.card-config-img__item.back')
   swapButton.addEventListener('click', () => {
@@ -38,8 +37,18 @@
       }, 150)
     }
   })
+}
+
+(cardConfigInit = () => {
+  const mainBlocks = document.querySelectorAll('.card-config')
+  for (elem of mainBlocks) {
+    fastOrder(elem)
+    rotatePhoto(elem)
+  }
 })();
 
+
+/*
 (switchSpecsTabs = () => {
   const tabs = document.querySelectorAll('.card-specs-tabs__item')
   const tables = document.querySelectorAll('.card-specs-tables-item')
@@ -54,7 +63,7 @@
     })
   }
 })();
-/*
+
 (initSpecsSlider = () => {
   if (document.documentElement.clientWidth <= 768) {
     const specsSlider = new Swiper('.card-specs-tables-item__slider.swiper-container', {
@@ -64,3 +73,20 @@
   }
 })();
 */
+
+(floatingBlockInit = () => {
+  const floatingBlockWrap = document.querySelector('.card-floating__wrapper')
+  const floatingBlock = floatingBlockWrap.querySelector('.card-floating')
+  let floatingBlockHeight = 132
+  if (document.documentElement.clientWidth <= 768) {
+    floatingBlockHeight = 172
+  }
+  window.addEventListener('scroll', () => {
+    if (floatingBlockWrap.getBoundingClientRect().y <= document.documentElement.clientHeight - floatingBlockHeight) {
+      floatingBlock.classList.remove('float')
+    }
+    else {
+      floatingBlock.classList.add('float')
+    }
+  })
+})();
