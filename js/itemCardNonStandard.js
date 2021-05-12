9c8d57cfaf132a1e486033d99fe192874b08b223
SkelVSH <51212766+SkelVSH@users.noreply.github.com>
2021-05-12 13:46:10 +0300

    feat: add config block to item-card-non-standard page

---
+++ js/itemCardNonStandard.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  (cardConfigInit = () => {
+    const configBlocks = document.querySelectorAll('.card-config')
+    for (elem of configBlocks) {
+      rotatePhoto(elem)
+      changeColor(elem)
+    }
+  })();
+})
