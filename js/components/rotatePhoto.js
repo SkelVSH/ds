763f5df5b1507274e0fbad5c5abc93e6a0a63d80
SkelVSH <51212766+SkelVSH@users.noreply.github.com>
2021-05-20 14:28:51 +0300

    refactor: change img rotation process

--- js/components/rotatePhoto.js
+++ js/components/rotatePhoto.js
@@ -7,26 +7,20 @@ rotatePhoto = ({ block, photoWrapSelector, swapButtonSelector, backImgSelector, 
   const backImg = photoWrap.querySelector(backImgSelector)
   const coverTitles = coverTitlesSelector ? block.querySelectorAll(coverTitlesSelector) : []
 
-  blockButton = () => {
-    swapButton.classList.add('disabled')
-    setTimeout(() => {
-      swapButton.classList.remove('disabled')
-    }, 300)
-  }
-
   swapButton.addEventListener('click', (e) => {
     e.stopImmediatePropagation()
-    blockButton()
     if (photoWrap.classList.contains('back_active')) {
       backImg.classList.remove('active')
       setTimeout(() => {
         photoWrap.classList.remove('back_active')
+        swapButton.nextElementSibling.innerHTML = "Отделка внутри"
       }, 150)
     }
     else {
       photoWrap.classList.add('back_active')
       setTimeout(() => {
         backImg.classList.add('active')
+        swapButton.nextElementSibling.innerHTML = "Отделка снаружи"
       }, 150)
     }
     for (elem of coverTitles) {
