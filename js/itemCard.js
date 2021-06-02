48fade4d3925f06b3aafc0421f280709c0c73075
SkelVSH <51212766+SkelVSH@users.noreply.github.com>
2021-06-02 14:48:51 +0300

    feat: implement double-door item-card block

--- js/itemCard.js
+++ js/itemCard.js
@@ -1,21 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
-
-  changeKit = (configBlock) => {
-    const kitsWrap = configBlock.querySelector('.card-config-main-kit__wrap')
-    const kits = kitsWrap.querySelectorAll('.card-config-main-kit-item')
-    kitsWrap.addEventListener('click', (e) => {
-      if (e.target.classList.contains('card-config-main-kit-item') && !e.target.classList.contains('active')) {
-        for (elem of kits) {
-          if (elem.classList.contains('active')) {
-            elem.classList.remove('active')
-            break
-          }
-        }
-        e.target.classList.add('active')
-      }
-    })
-  }
 
   /* ПЕРЕРАБОТАТЬ ЭТУ ЧАСТЬ ПОСЛЕ НАТЯЖКИ ИЛИ УБРАТЬ */
   (formInteraction = () => {
